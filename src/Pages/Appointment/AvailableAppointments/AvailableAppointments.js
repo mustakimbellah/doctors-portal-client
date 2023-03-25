f163c029865f93ce7609090155457ef41858f46b
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2023-03-26 03:53:51 +0600

    appointment booking related work

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -3,23 +3,30 @@ import { format } from 'date-fns';
 import AppointmentOptions from './AppointmentOptions';
 import BookingModal from '../BookingModal/BookingModal';
 import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading/Loading';
 
 const AvailableAppointments = ({ selectedDate }) => {
 
     const [treatment, setTreatment] = useState(null);
 
-    const { data: appointmentOptions = [] } = useQuery({
-        queryKey: ['appointmentOptions'],
+    const date = format(selectedDate, 'PP');
+
+    const { data: appointmentOptions = [], refetch, isLoading } = useQuery({
+        queryKey: ['appointmentOptions', date],
 
         // queryFn: () => fetch('http://localhost:5000/appointmentOptions')
         // .then(res => res.json())
 
         queryFn: async () => {
-            const res = await fetch('http://localhost:5000/appointmentOptions');
+            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
             const data = await res.json();
             return data;
         }
     });
+
+    if(isLoading){
+        return <Loading></Loading>
+    }
 
 
     return (
@@ -43,6 +50,7 @@ const AvailableAppointments = ({ selectedDate }) => {
                     selectedDate={selectedDate}
                     treatment={treatment}
                     setTreatment={setTreatment}
+                    refetch={refetch}
                 ></BookingModal>
             }
 
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -3,8 +3,8 @@ import React, { useContext } from 'react';
 import { toast } from 'react-hot-toast';
 import { AuthContext } from '../../../contexts/AuthProvider';
 
-const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
-    const { name, slots } = treatment;
+const BookingModal = ({ treatment, selectedDate, setTreatment, refetch }) => {
+    const { name: treatmentName, slots } = treatment;
     const date = format(selectedDate, 'PP');
 
     const { user } = useContext(AuthContext);
@@ -19,7 +19,7 @@ const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
 
         const booking = {
             appointmentData: date,
-            treatment: name,
+            treatment: treatmentName,
             patient: name,
             slot,
             email,
@@ -39,7 +39,11 @@ const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
                 console.log(data);
                 if (data.acknowledged) {
                     setTreatment(null);
-                    toast.success('Booking Confirmed')
+                    toast.success('Booking Confirmed');
+                    refetch();
+                }
+                else {
+                    toast.error(data.message);
                 }
 
             })
@@ -54,7 +58,7 @@ const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{name}</h3>
+                    <h3 className="text-lg font-bold">{treatmentName}</h3>
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
 
                         <input type="text" disabled value={date} className="input input-bordered w-full " />
